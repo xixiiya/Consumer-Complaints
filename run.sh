a8acd9f85c99eef0296fe123cb684d8e9e70a90e
Xixi(Cici) Kang <xixikang96@gmail.com>
2020-05-19 10:51:31 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 ﻿#!/bin/bash
 # BuildAndRun.sh
-# Compile java code
+
 
 file="complaints.java"
 
-# src directory
+
 cd src
 
 class=$(echo $file | awk -F '.' '{print $1}')
@@ -13,14 +13,14 @@ echo "Start Compile"
 echo $class
 echo "------------------------------------"
 
-# Compile
+
 javac $file
 
 if [ $? -eq 0 ]; then
     echo "Success.Start execute"
     echo "------------------------------------"
 
-    # Execute
+
     java $class
     if [ $? -eq 0 ]; then
         echo "------------------------------------"
@@ -34,7 +34,7 @@ else
     echo "Compile error"
 fi
 
-# Delete class file
+
 if [ -f $class.class ]; then
     rm -rf $class.class
 fi
