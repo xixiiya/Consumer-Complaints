3b3fef6d530bbb93306c87f1bc364fd488fef15f
Xixi(Cici) Kang <xixikang96@gmail.com>
2020-05-19 10:48:00 -0400

    Add files via upload

---
+++ run.sh
@@ -0,0 +1,40 @@
+﻿#!/bin/bash
+# BuildAndRun.sh
+# Compile java code
+
+file="complaints.java"
+
+# src directory
+cd src
+
+class=$(echo $file | awk -F '.' '{print $1}')
+
+echo "Start Compile"
+echo $class
+echo "------------------------------------"
+
+# Compile
+javac $file
+
+if [ $? -eq 0 ]; then
+    echo "Success.Start execute"
+    echo "------------------------------------"
+
+    # Execute
+    java $class
+    if [ $? -eq 0 ]; then
+        echo "------------------------------------"
+        echo "Finish execute"
+    else
+        echo "------------------------------------"
+        echo "Execute error"
+    fi
+else
+    echo "------------------------------------"
+    echo "Compile error"
+fi
+
+# Delete class file
+if [ -f $class.class ]; then
+    rm -rf $class.class
+fi
